e88b2eb01e387759cb1b675d9e5dc0a650476834
Ronin Vohra <vohrar@uci.edu>
2023-12-17 16:59:25 -0800

    image for place component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-import Place from './components/place.js'
+import React from 'react';
+import Place from './components/place.js';
+
 function App() {
   return (
-    <div className="App">
-      <h1> Things2do </h1>
-      <Place />
-      <Place />
-      <Place />
+    <div className="App p-4 flex flex-col items-center">
+      <h1 className="text-2xl font-bold mb-4">Things2do</h1>
+      <div className="w-full md:w-1/2 mx-auto">
+        <div className="w-full p-4 border border-gray-300 rounded mb-4">
+          <h2 className="text-xl font-bold mb-2">Breakfast</h2>
+          <Place />
+        </div>
+        <div className="w-full p-4 border border-gray-300 rounded mb-4">
+          <h2 className="text-xl font-bold mb-2">Lunch</h2>
+          <Place />
+        </div>
+        <div className="w-full p-4 border border-gray-300 rounded mb-4">
+          <h2 className="text-xl font-bold mb-2">Activity</h2>
+          <Place />
+        </div>
+        <div className="w-full p-4 border border-gray-300 rounded mb-4">
+          <h2 className="text-xl font-bold mb-2">Snack</h2>
+          <Place />
+        </div>
+        <div className="w-full p-4 border border-gray-300 rounded mb-4">
+          <h2 className="text-xl font-bold mb-2">Dinner</h2>
+          <Place />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/place.js
+++ src/components/place.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
-
+import cafe from '../images/cafe.jpeg'
 const Place = () => {
   const places = [
     {
       name: 'Cafe Sunshine',
       rating: '5 stars',
       location: 'New York',
-      openTimes: '8:00 AM - 10:00 PM',
+      openTimes: '8:00 AM - 10:00 PM'
     },
     {
       name: 'Spicy Bites',
       rating: '4.5 stars',
       location: 'Paris',
-      openTimes: '11:00 AM - 9:00 PM',
+      openTimes: '11:00 AM - 9:00 PM'
     },
   ];
 
@@ -29,17 +29,24 @@ const Place = () => {
 
   return (
     <div className="flex justify-center items-center">
-      <div className="p-4 border border-gray-300 rounded text-center m-4">
-        <h2 className="text-xl font-bold mb-2">{currentPlace.name}</h2>
-        <p className="text-gray-600">Rating: {currentPlace.rating}</p>
-        <p className="text-gray-600">Location: {currentPlace.location}</p>
-        <p className="text-gray-600">Open Times: {currentPlace.openTimes}</p>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
-          onClick={changePlace}
-        >
-          Change Place
-        </button>
+      <div className="p-4 border border-gray-300 rounded text-center m-4 flex items-center">
+        <img
+          src={cafe} // Replace with actual image URL
+          alt={currentPlace.name}
+          className="w-20 h-20 object-cover rounded-full mr-4"
+        />
+        <div>
+          <h2 className="text-xl font-bold mb-2">{currentPlace.name}</h2>
+          <p className="text-gray-600">Rating: {currentPlace.rating}</p>
+          <p className="text-gray-600">Location: {currentPlace.location}</p>
+          <p className="text-gray-600">Open Times: {currentPlace.openTimes}</p>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+            onClick={changePlace}
+          >
+            Change Place
+          </button>
+        </div>
       </div>
     </div>
   );
